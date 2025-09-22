7bca1775d4e7ed9e2f56d6dda24240ab9cb2ce89
Kanna Plamoottil <dev.jjplamoottilaws@gmail.com>
2025-09-22 02:06:07 -0500

    fix error {semantically correct blocks are used for components. No <div /> or other unnecessary HTML tags are used for components that consist of single-level blocks. - this is an unnecessary wrapper)

--- web/src/components/copyright/copyright.jsx
+++ web/src/components/copyright/copyright.jsx
@@ -2,11 +2,9 @@ import "./copyright.css";
 
 const Copyright = () => {
   return (
-    <div className="copyright">
-      <p className="copyright__text">
-        All Rights Reserved™ Copyright© {new Date().getFullYear()}®
-      </p>
-    </div>
+    <p className="copyright__text">
+      All Rights Reserved™ Copyright© {new Date().getFullYear()}®
+    </p>
   );
 };
 
